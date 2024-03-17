e2f5c5f65ba08b446f25e1dff4cae77981fce24c
fllullk <fallull@uc.cl>
2024-03-17 20:36:22 -0300

    Subiendo solucion problema tres tiempo

--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,17 @@
 from typing import List, Tuple
+from collections import defaultdict
+import pandas as pd
+import heapq
+import ujson
+import re
+
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    conteo = defaultdict(int)
+    with open(file_path, 'r') as file:
+        for line in file:
+            linea = ujson.loads(line)
+            for user in re.findall(r"@(\S+?) ", linea["content"]):
+                conteo[user] += 1
+
+    return heapq.nlargest(10, conteo.items(), key=lambda x: x[1])
