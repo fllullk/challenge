1f1d77faef66e8d9e79d11df542de67b2aac1382
fllullk <fallull@uc.cl>
2024-03-17 20:35:22 -0300

    Subiendo solucion problema dos memoria

--- src/q2_memory.py
+++ src/q2_memory.py
@@ -1,4 +1,18 @@
 from typing import List, Tuple
+import pandas as pd
+import emoji
+import ujson
+
+
+def read_jsonl_fast(file_path):
+    data = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            data.append(ujson.loads(line))
+    return pd.DataFrame(data)
 
 def q2_memory(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    data = read_jsonl_fast(file_path)
+    matches = pd.Series([i['emoji'] for i in emoji.emoji_list(data["content"].str.cat())])
+    sol = [(row['index'], row['count']) for row in matches.value_counts().nlargest(10).reset_index().to_dict('records')]
+    return sol
